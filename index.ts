4e57a90ff74917e09b3086202e43f608ffec1697
Kevin Jiang <KevinSJ@users.noreply.github.com>
2023-08-14 20:36:50 +1200

    initial commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import { test as test0 } from './&&/../module';
+import { test as test1 } from './$$/../module';
+import { test as test2 } from './%%/../module';
+import { test as test3 } from './33/../module';
+import { test as test4 } from './,,/../module';
+import { test as test5 } from './abcdefg/../module';
+
+console.log(test0);
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
